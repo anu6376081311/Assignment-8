6fb9f569291d91ecbf71316faebc07c5398cb833
anumeena <111556950+anu6376081311@users.noreply.github.com>
2023-03-14 23:00:04 +0530

    Add files via upload

---
+++ A8(14).c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=0;i<5;i++)
+    {
+        for(j=0;j<5;j++)
+        {
+            if(j==0 || i==j ||i ==4)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
